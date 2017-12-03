91ec7de4ea16f657073eaf01aa3884f41a41ac26
PolinaGusenkova <polinagpa@gmail.com>
2017-12-04 00:10:16 +0300

    Swipe and buttons

--- app/src/main/java/com/example/www/goldmineproject/GroupActivity.java
+++ app/src/main/java/com/example/www/goldmineproject/GroupActivity.java
@@ -4,50 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.GestureDetectorCompat;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.RelativeLayout;
+import android.view.View;
 
-import java.security.acl.Group;
-
-public class MainActivity extends AppCompatActivity {
-
-    private OnSwipeTouchListener onSwipeTouchListener;
+public class GroupActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_group);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        onSwipeTouchListener = new OnSwipeTouchListener(MainActivity.this) {
-            public void onSwipeRight() {
-                Intent intent = new Intent(MainActivity.this, GroupActivity.class);
-                startActivity(intent);
-            }
-            public void onSwipeLeft() {
-                Intent intent = new Intent(MainActivity.this, GroupActivity.class);
-                startActivity(intent);
-            }
-        };
-
-        RelativeLayout myLayout = (RelativeLayout) findViewById(R.id.mainRelLayout);
-        myLayout.setOnTouchListener(onSwipeTouchListener);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddPersonalActivity.class);
+                Intent intent = new Intent(GroupActivity.this, AddGroupActivity.class);
                 startActivity(intent);
             }
         });
@@ -74,6 +50,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 
 }
