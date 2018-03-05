0ffc4a195a8aa4bcb23c2a783252b4512b95c76f
AdeliaSabirova <adeliafsabirova@gmail.com>
2018-03-05 15:52:55 +0300

    test

--- app/src/main/java/com/example/www/goldmineproject/EditGroupActivity.java
+++ app/src/main/java/com/example/www/goldmineproject/EditGroupActivity.java
@@ -6,13 +6,16 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import io.realm.Realm;
+
 /**
  * Created by Аделя on 28.02.2018.
  */
 
 public class EditGroupActivity extends AppCompatActivity {
 
-    TextView groupTextView;
+    private TextView groupTextView;
+    private Realm realm;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
